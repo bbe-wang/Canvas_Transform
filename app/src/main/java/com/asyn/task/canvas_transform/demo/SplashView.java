9e5c5484f60768a4aa8df5ded57e594feadabacc
caozenghui <caozenghui@langyue.org>
2019-11-18 10:50:35 +0800

    ccc

---
+++ app/src/main/java/com/asyn/task/canvas_transform/demo/SplashView.java
@@ -0,0 +1,32 @@
+package com.asyn.task.canvas_transform.demo;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by ThinkPad on 2019/11/18.
+ */
+
+public class SplashView extends View{
+
+
+    
+
+    public SplashView(Context context) {
+        super(context);
+    }
+
+    public SplashView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SplashView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+}
